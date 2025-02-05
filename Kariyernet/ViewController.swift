1f882e74438a4755c33441df0401dff31c2c3040
Canberk Çakmak <canberkcakmak@ogoodigital.com>
2025-02-06 02:47:21 +0300

    Initial Commit

---
+++ Kariyernet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kariyernet
+//
+//  Created by Canberk Çakmak on 6.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
